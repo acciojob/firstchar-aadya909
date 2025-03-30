83ae10d650de842f76adbd61a8cc09319821dd6d
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-30 22:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstChar(text) {
-  // your code here
+  for (let char of text) {
+    if (char !== ' ') {
+      return char;
+    }
+  }
+  return '';
 }
 
+// Test cases
+console.log(firstChar(' Rosa Parks ')); // 'R'
+console.log(firstChar(' Hello World ')); // 'H'
+console.log(firstChar(' ')); // ''
+console.log(firstChar('')); // ''
+
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+ const text = prompt("Enter text:");
 alert(firstChar(text));
+
